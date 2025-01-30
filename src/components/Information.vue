44f50b626716c9e44a013470b69de13bce1d4bde
ictye <northgreen2006@qq.com>
2025-01-30 16:33:13 +0800

    update

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -7,16 +7,10 @@ let link = "https://raw.githubusercontent.com/northgreen/zyn-record/refs/heads/m
 
 let data = ref(null)
 let l = ref(null)
-const config = {
-  headers: {
-    'Cache-Control': 'no-cache',
-    'Pragma': 'no-cache',
-    'Expires': '0'
-  }
-};
+
 const fetchData = async () => {
   try {
-    const response = await axios.get(link,config);
+    const response = await axios.get(link);
     data.value = response.data.data;
     l.value = response.data.data.length;
   } catch (error) {
